b1dd64dbdf71e7646fd639fd51fd066defcc86fe
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-07-06 10:38:39 +0530

    Added functonality for algorithm programs

---
+++ src/com/bridgelabz/algorithm/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.algorithm;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        BubbleSort sort = new BubbleSort();
+        int array[] = {3,7,2,9,5,1,4,0};
+        sort.bubbleSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public void bubbleSort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > (array[j + 1])) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+}
